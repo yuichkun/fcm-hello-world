c2be2a0bb10b5575329cc3201060140ac8404e15
yuichkun <yuichkun@gmail.com>
2019-07-23 14:17:45 +0900

    feat-client: get token from firebase messaging

--- src/app.js
+++ src/app.js
@@ -4,3 +4,14 @@ import config from '../firebase.config'
 firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
+messaging.usePublicVapidKey(process.env.VAPID)
+
+Notification.requestPermission().then(async (permission) => {
+  if (permission === 'granted') {
+    console.log('Notification permission granted.');
+    const token = await messaging.getToken();
+    console.log(token);
+  } else {
+    console.log('Unable to get permission to notify.');
+  }
+});
